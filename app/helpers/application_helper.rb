62567c69a0a4383e2712c49e370d460ac3021cea
Nelson Dornelas <ndornelasj@gmail.com>
2017-12-17 17:00:56 -0800

    view refactoring, bootstrap included with custom.scss, jquery.turbolinks added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def header(text)
     content_for(:header) { text.to_s }
   end
+
+  def full_title(page_title = '')
+    base_title = "Devise Demo"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
