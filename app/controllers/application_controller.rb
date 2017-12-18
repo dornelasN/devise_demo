01250a46d3fcf7e003ed77b270ea2216d62853fa
Nelson Dornelas <ndornelasj@gmail.com>
2017-12-17 18:04:57 -0800

    edit user name attribute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # whitelist the :name attribute for sign_up and account_update actions
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   def full_title(page_title = '')
-    base_title = "Devise Demo"
+    base_title = 'Devise Demo'
     if page_title.empty?
       base_title
     else
